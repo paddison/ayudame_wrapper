6fd636c3781f64a5dd465fcd587af37a794ecd6e
paddison <62658917+paddison@users.noreply.github.com>
2022-09-13 16:37:15 +0200

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
     // add ayudame to library search path
-    println!("cargo:rustc-link-search=/home/patrick/hlrs/rust_rewrite/helpers/lib");
+    println!("cargo:rustc-link-search=PATH/TO/AYUDAME");
     println!("cargo:rustc-link-lib=ayudame")
 }
